d767f2b03d3dbb1a17b89e691a3de19471837489
BartholomewBaron <2085713218@qq.com>
2022-06-11 06:58:30 +0800

    Initial commit

---
+++ javaweb/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 可乐
+  Date: 2022/6/9
+  Time: 17:06
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>尚硅谷</title>
+  </head>
+  <body>
+    <h1 style="color: aqua">姚训涵</h1>
+  </body>
+</html>
---
+++ src/dome.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+
+public class dome {
+    public static void main(String[] args) {
+        System.out.println();
+
+
+        Date date = new Date();
+        for (String arg : args) {
+            
+        }
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            
+        }
+    }
+
+}
